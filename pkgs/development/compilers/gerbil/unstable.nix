3f70a76c2381d37a6bd0d00feecec41879b9f6b4
Francois-Rene Rideau <fare@tunes.org>
2021-01-14 12:17:36 -0500

    gerbil-unstable: 2020-11-05 -> 2023-03-23

--- pkgs/development/compilers/gerbil/unstable.nix
+++ pkgs/development/compilers/gerbil/unstable.nix
@@ -1,13 +1,14 @@
 { callPackage, fetchFromGitHub, gambit-unstable, gambit-support }:
 
 callPackage ./build.nix rec {
-  version = "unstable-2020-11-05";
-  git-version = "0.16-152-g808929ae";
+  version = "unstable-2023-03-23";
+  git-version = "0.17.0-78-gc0d6e529";
   src = fetchFromGitHub {
-    owner = "vyzo";
+    #owner = "vyzo";
+    owner = "drewc";
     repo = "gerbil";
-    rev = "808929aeb8823959191f35df53bc0c0150911b4b";
-    sha256 = "0d9k2gkrs9qvlnk7xa3gjzs3gln3ydds7yd2313pvbw4q2lcz8iw";
+    rev = "c0d6e529c55e91e41fa924afdfb48bc77266f66a";
+    sha256 = "187sfa1rjraln4f0ga1rk2mwa3hw5phdv2938870gc0c71avw4m1";
   };
   inherit gambit-support;
   gambit = gambit-unstable;
