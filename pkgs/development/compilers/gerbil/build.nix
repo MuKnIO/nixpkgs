9bec69e6e80b377f04e6071bf05cccb02d7a3091
Francois-Rene Rideau <fare@tunes.org>
2023-12-12 20:23:09 -0500

    gerbil-unstable: 2023-12-06 -> 2025-11-15

--- pkgs/development/compilers/gerbil/build.nix
+++ pkgs/development/compilers/gerbil/build.nix
@@ -4,6 +4,7 @@
   openssl,
   zlib,
   sqlite,
+  # git, openssh,
   version,
   git-version,
   src,
@@ -31,12 +32,12 @@ stdenv.mkDerivation rec {
   # or give up and delete all tentative support for static libraries.
   #buildInputs_staticLibraries = map makeStaticLibraries buildInputs_libraries;
 
-  buildInputs = buildInputs_libraries;
+  buildInputs = buildInputs_libraries; # ++ [ git openssh ];
 
   postPatch = ''
     patchShebangs . ;
     grep -Fl '#!/usr/bin/env' `find . -type f -executable` | while read f ; do
-      substituteInPlace "$f" --replace '#!/usr/bin/env' '#!${coreutils}/bin/env' ;
+      substituteInPlace "$f" --replace-warn '#!/usr/bin/env' '#!${coreutils}/bin/env' ;
     done ;
     cat > MANIFEST <<EOF
     gerbil_stamp_version=v${git-version}
@@ -44,10 +45,10 @@ stdenv.mkDerivation rec {
     gambit_stamp_ymd=${gambit-stampYmd}
     gambit_stamp_hms=${gambit-stampHms}
     EOF
-    for f in src/bootstrap/gerbil/compiler/driver__0.scm \
+    for f in src/bootstrap/gerbil/compiler/driver*0.scm \
              src/build/build-libgerbil.ss \
              src/gerbil/compiler/driver.ss ; do
-      substituteInPlace "$f" --replace '"gcc"' '"${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}gcc"' ;
+      substituteInPlace "$f" --replace-warn '"gcc"' '"${stdenv.cc}/bin/${stdenv.cc.targetPrefix}gcc"' ;
     done
   '';
 
@@ -66,7 +67,7 @@ stdenv.mkDerivation rec {
     "--prefix=$out/gerbil"
     "--enable-zlib"
     "--enable-sqlite"
-    "--enable-shared"
+    # "--enable-shared" # Not for v0.18.2 and later...
     "--enable-march=" # Avoid non-portable invalid instructions. Use =native if local build only.
   ];
 
@@ -75,11 +76,11 @@ stdenv.mkDerivation rec {
     "-Wno-error=implicit-function-declaration";
 
   configurePhase = ''
-    export CC=${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}gcc \
-           CXX=${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}g++ \
-           CPP=${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}cpp \
-           CXXCPP=${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}cpp \
-           LD=${gccStdenv.cc}/bin/${gccStdenv.cc.targetPrefix}ld \
+    export CC=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}gcc \
+           CXX=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}g++ \
+           CPP=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cpp \
+           CXXCPP=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cpp \
+           LD=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}ld \
            XMKMF=${coreutils}/bin/false
     unset CFLAGS LDFLAGS LIBS CPPFLAGS CXXFLAGS
     ./configure ${builtins.concatStringsSep " " configureFlags}
@@ -95,6 +96,7 @@ stdenv.mkDerivation rec {
     runHook preBuild
 
     # gxprof testing uses $HOME/.cache/gerbil/gxc
+    echo NIX_BUILD_CORES=$NIX_BUILD_CORES
     export HOME=$PWD
     export GERBIL_BUILD_CORES=$NIX_BUILD_CORES
     export GERBIL_GXC=$PWD/bin/gxc
@@ -107,24 +109,25 @@ stdenv.mkDerivation rec {
     # Build, replacing make by build.sh
     ( cd src && sh build.sh )
 
-    f=build/lib/libgerbil.so.ldd ; [ -f $f ] && :
-    substituteInPlace "$f" --replace '(' \
+    f=(build/lib/libgerbil*.ldd) ; [ -f $f[0] ] &&
+    substituteInPlace "$f" --replace-warn '(' \
       '(${lib.strings.concatStrings (map (x: "\"${x}\" ") extraLdOptions)}'
 
     runHook postBuild
   '';
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/gerbil $out/bin
-    ./install.sh
-    (cd $out/bin ; ln -s ../gerbil/bin/* .)
-    runHook postInstall
-  ''
-  + lib.optionalString stdenv.hostPlatform.isDarwin ''
-    libgerbil="$(realpath "$out/gerbil/lib/libgerbil.so")"
-    install_name_tool -id "$libgerbil" "$libgerbil"
-  '';
+  installPhase =
+    ''
+      runHook preInstall
+      mkdir -p $out/gerbil $out/bin
+      ./install.sh
+      (cd $out/bin ; ln -s ../gerbil/bin/* .)
+      runHook postInstall
+    '';
+    ##+ lib.optionalString stdenv.hostPlatform.isDarwin ''
+    ##  libgerbil="$(realpath "$out/gerbil/lib/libgerbil.dylib")"
+    ##  install_name_tool -id "$libgerbil" "$libgerbil"
+    ##'';
 
   dontStrip = true;
 
--- pkgs/development/compilers/gerbil/unstable.nix
+++ pkgs/development/compilers/gerbil/unstable.nix
@@ -5,19 +5,19 @@
 }:
 
 callPackage ./build.nix rec {
-  version = "unstable-2023-12-06";
-  git-version = "0.18.1";
+  version = "unstable-2025-11-15";
+  git-version = "0.18.1-172-g0cee64de";
   src = fetchFromGitHub {
     owner = "mighty-gerbils";
     repo = "gerbil";
-    rev = "23c30a6062cd7e63f9d85300ce01585bb9035d2d";
-    sha256 = "15fh0zqkmnjhan1mgymq5fgbjsh5z9d2v6zjddplqib5zd2s3z6k";
+    rev = "0cee64de38756ed9198d3493d4c7afddfa6e04e1";
+    sha256 = "0xhkk3isarigf2ly0n32rma9br1ywb5mfhaby815f95fn2nmm1pm";
     fetchSubmodules = true;
   };
   inherit gambit-support;
   gambit-params = gambit-support.unstable-params;
   # These are available in pkgs.gambit-unstable.passthru.git-version, etc.
-  gambit-git-version = "4.9.5-78-g8b18ab69";
+  gambit-git-version = "4.9.7-6-g64f4d369";
   gambit-stampYmd = "20231029";
   gambit-stampHms = "163035";
 }
